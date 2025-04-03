1ff48702fa86e677bbd99f0b9d50a383f26ec630
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-04-03 22:13:45 +0300

    Add length and append methods

--- CircularSinglyLinkedList.js
+++ CircularSinglyLinkedList.js
@@ -15,6 +15,26 @@ class CircularSinglyLinkedList {
         this.tail = null;
         this.size = 0;
     }
+
+    length() {
+        return this.size;
+    }
+
+    append(element) {
+        if (typeof element !== 'string' || element.length !== 1) {
+            throw new Error('Element must be a single character.');
+        }
+        const newNode = new Node(element);
+        if (!this.tail) {
+            newNode.next = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.tail.next;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+    }
 }
 
 module.exports = CircularSinglyLinkedList;
