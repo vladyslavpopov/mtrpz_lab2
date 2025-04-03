7a8f6429b6f512a68165c561b6093890d74a6ae9
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-04-03 21:48:33 +0300

    Add tests for insert and delete methods

--- testArrayBasedList.js
+++ testArrayBasedList.js
@@ -9,4 +9,11 @@ const ArrayBasedList = require('./ArrayBasedList');
     list.append('B');
     list.append('C');
     assert.strictEqual(list.length(), 3, 'append() or length() are not working correctly');
+
+    list.insert('D', 1); // Expected list: A, D, B, C
+    assert.strictEqual(list.get(1), 'D', 'insert() is not working correctly');
+
+    const deleted = list.delete(2); // Removing 'B'; expected list: A, D, C
+    assert.strictEqual(deleted, 'B', 'delete() is not working correctly');
+    assert.strictEqual(list.length(), 3, 'delete() did not update the list length correctly');
 })();
