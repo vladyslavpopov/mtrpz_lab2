c0d892e2cd9c42fb07a751af486c77bb671c9ed0
Vladyslav Popov <popovvladislav4891@gmail.com>
2025-04-03 21:46:22 +0300

    Add tests for append and lengths methods

--- testArrayBasedList.js
+++ testArrayBasedList.js
@@ -1,1 +1,12 @@
+const assert = require('assert');
+const ArrayBasedList = require('./ArrayBasedList');
 
+(function testArrayBasedList() {
+    console.log('Testing ArrayBasedList...');
+    const list = new ArrayBasedList();
+
+    list.append('A');
+    list.append('B');
+    list.append('C');
+    assert.strictEqual(list.length(), 3, 'append() or length() are not working correctly');
+})();
